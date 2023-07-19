cddef801a50f900ade8eed4853973f80e9bd9cfc
Mehmet Emre Kutluca <mehmetemrekutluca@gmail.com>
2023-07-19 23:02:51 +0300

    Mail

--- mail.js
+++ mail.js
@@ -1,17 +1,20 @@
 function sendMail() {
+    console.log("send mail called")
     const key = "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"
 
-const url = "https://api.sendgrid.com/v3/mail/send";
+    const url = "https://api.sendgrid.com/v3/mail/send";
 
-const options = {
+    const options = {
   headers: {
     Authorization: "Bearer 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",
     "Content-Type": "application/json"
   },
   data: '{"personalizations": [{"to": [{"email": "mehmetemrekutluca@gmail.com"}]}],"from": {"email": "test@example.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'
-};
+    };
 
-fetch(url, options)
-  .then( res => res.json() )
-  .then( data => console.log(data) );
+    fetch(url, options)
+     .then( res => res.json() )
+     .then( data => console.log(data) )
+     .catch(err => console.log(err));
+     ;
 }
